e4e6ba3512018b83fd340b7e2d6ee519c91e0260
amandam2017 <amandamaarman19911@gmail.com>
2022-03-29 16:21:07 +0200

    I have created select and option elements on index.js and now I am able to display the dropdown with its data....now I need to make the submit button work

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // referenced the html element
 const carsElem = document.querySelector('.cars');
 
+const carBrandsElem = document.querySelector('.carBrands');
+const carColorsElem = document.querySelector('.carColors');
+const btnElem = document.querySelector('.btn');
+
 // axios link and display data
 axios.get("https://api-tutor.herokuapp.com/v1/cars").then(function(result){
     console.log(result.data);
@@ -15,5 +19,29 @@ axios.get("https://api-tutor.herokuapp.com/v1/cars").then(function(result){
         </tr>`
     carsElem.appendChild(tableElement);
 
+    });
+})
+
+axios.get("https://api-tutor.herokuapp.com/v1/cars").then(function(result){
+    console.log(result.data);
+    
+    result.data.forEach(eachCar => {
+        const filterElem = document.createElement('option');
+    
+        filterElem.innerHTML = `<select>
+            <option>${eachCar.make}</option>
+        </select>`
+        carBrandsElem.appendChild(filterElem);
+        
+    });
+
+    result.data.forEach(eachCar => {
+        const filterElem = document.createElement('option');
+    
+        filterElem.innerHTML = `<select>
+            <option>${eachCar.color}</option>
+        </select>`
+        carColorsElem.appendChild(filterElem);
+        
     });
 })
