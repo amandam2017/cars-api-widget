592b94d374755b25f750dff97187548522226ca4
amandam2017 <amandamaarman19911@gmail.com>
2022-03-29 15:14:00 +0200

    added headers

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ axios.get("https://api-tutor.herokuapp.com/v1/cars").then(function(result){
 
     result.data.forEach(car => {
         const li = document.createElement('tr');
+        
         li.innerHTML = `<tr>
             <td>${car.model}</td>
             <td>${car.color}</td>
