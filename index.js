c9229e3e18396e6a91c4904e3a3f1afffb339dab
amandam2017 <amandamaarman19911@gmail.com>
2022-03-29 15:17:16 +0200

    trying to style page

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ axios.get("https://api-tutor.herokuapp.com/v1/cars").then(function(result){
     console.log(result.data);
 
     result.data.forEach(car => {
-        const li = document.createElement('tr');
+        const tableElement = document.createElement('tr');
         
-        li.innerHTML = `<tr>
+        tableElement.innerHTML = `<tr>
             <td>${car.model}</td>
             <td>${car.color}</td>
             <td>${car.make}</td>
         </tr>`
-    carsElem.appendChild(li);
+    carsElem.appendChild(tableElement);
 
     });
 })
