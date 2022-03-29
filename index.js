4fa1f20e7c7d3d08bcda6e1d5e416797e68ea659
amandam2017 <amandamaarman19911@gmail.com>
2022-03-29 14:54:40 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// referenced the html element
+const carsElem = document.querySelector('.cars');
+
+// axios link and display data
+axios.get("https://api-tutor.herokuapp.com/v1/cars").then(function(result){
+    console.log(result.data);
+
+    result.data.forEach(car => {
+        const li = document.createElement('tr');
+        li.innerHTML = `<tr>
+            <td>${car.model}</td>
+            <td>${car.color}</td>
+            <td>${car.make}</td>
+        </tr>`
+    carsElem.appendChild(li);
+
+    });
+})
